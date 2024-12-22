9710cbf0fdfc88881553c0208d2a6e644d21144d
DennisDHBW <den.gerhardt.24@lehre.mosbach.dhbw.de>
2024-12-22 12:19:05 +0100

    project init

--- src/models/Monster.py
+++ src/models/Monster.py
@@ -4,9 +4,26 @@ class Monster:
         self.__name = name
         self.__strength = strength
         self.__health = health
+        self.__healthInit = health
 
     def __str__(self):
         print(f"Monster {self.__name} has {self.__strength} strength and {self.__health} health.")
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def health(self):
+        return self.__health
+
+    @property
+    def strength(self):
+        return self.__strength
+
+    @property
+    def health_init(self):
+        return self.__healthInit
 
     def take_damage(self, damage):
         self.__health -= damage
--- src/models/Room.py
+++ src/models/Room.py
@@ -1,14 +1,39 @@
 from src.models.Player import Player
+from src.models.Monster import Monster
 
 class Room:
 
-    def __init__(self, description, monster):
+    def __init__(self, description, monster: Monster):
         self.__description = description
         self.__monster = monster
 
     def __str__(self):
         print(self.__description)
 
-    def fight_round(self, player):
-        pass
 
+    def interact(self, player: Player):
+        print(f"Welcome to the {self.__description}")
+        "# TODO"
+
+    def fight_round(self, player: Player):
+        monster = self.__monster
+
+        while monster.health > 0:
+
+            print(f"The monster hits you with {monster.strength}  strength.")
+            player.take_damage(monster.strength)
+            print(player)
+
+            if player.health <= 0:
+                print("You lost the fight")
+                break
+
+            print(f"You hit the monster with {player.strength} strength.")
+            monster.take_damage(player.strength)
+            print(monster)
+
+        if monster.health <= 0:
+            print("The monster is dead")
+            regain_health = int(monster.health_init / 2)
+            player.regain_health(regain_health)
+            print(f"Player {player.name}, you regained health: {player.health}")
